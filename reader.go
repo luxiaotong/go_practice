59da9bfeb51ebb0523854c0f898085a9c68fcc93
逯晓瞳 <shannon@luxiaotongdeMacBook-Pro-2.local>
2016-12-07 17:11:09 +0800

    Add reader sample

---
+++ reader.go
@@ -0,0 +1,20 @@
+package main
+import (
+    "fmt"
+    "io"
+    "strings"
+)
+
+func main() {
+    r := strings.NewReader("Hello, Reader!")
+    b := make([]byte, 8)
+    for {
+        n, err := r.Read(b)
+        if err == io.EOF {
+            break
+        }
+
+        fmt.Printf("n = %v, err = %v, b = %v\n", n, err, b)
+        fmt.Printf("b[:n] = %q\n", b[:n])
+    }
+}
