e0d97d7ffe16100e45ac0a687d7f08a5ddca49a9
逯晓瞳 <shannon@luxiaotongdeMacBook-Pro-2.local>
2016-12-08 09:34:49 +0800

    Add cal samples

---
+++ cal1.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+import "time"
+
+func main(){
+    t1 := time.Now()
+    count :=0
+    for i :=0; i< 9000000000; i++{
+        count += i
+    }
+    t2 := time.Now()
+    fmt.Printf("cost:%d,count:%d\n",t2.Sub(t1),count)
+}
+
