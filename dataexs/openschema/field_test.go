abdb9787ed1dd0501c73020eeeb14a1d71a6bbd8
luxiaotong <luxiaotong713@sina.com>
2021-05-21 15:18:54 +0800

    openschema test vote done

--- dataexs/openschema/field_test.go
+++ dataexs/openschema/field_test.go
@@ -57,20 +57,30 @@ func testFillField(t *testing.T) {
 			WithJSON(req).Expect().Status(http.StatusOK)
 		fmt.Printf("/field/fill %v response: %v\n", id, resp.Body())
 	}
-
 }
 
 func testVoteField(t *testing.T) {
-	id, _ := strconv.ParseInt(fieldID, 10, 64)
-	req := &VoteFieldRequest{
-		ID:   id,
-		Type: 20,
-	}
-	resp := e.PUT("/field/vote").
+	id, _ := strconv.ParseInt(dictID, 10, 64)
+	req := &GetFieldsRequest{id}
+	resp := e.POST("/dict/fields").
 		WithHeader("Authorization", "Bearer "+token).
 		WithCookie(CookieSecret, cookieVal).
 		WithJSON(req).Expect().Status(http.StatusOK)
-	fmt.Printf("/field/vote response: %v\n", resp.Body())
+	list := resp.JSON().Object().Value("data").Object().Value("list").Array()
+	for _, val := range list.Iter() {
+		fieldID := val.Object().Value("id").String().Raw()
+		fmt.Println("field id: ", fieldID)
+		id, _ := strconv.ParseInt(fieldID, 10, 64)
+		req := &VoteFieldRequest{
+			ID:   id,
+			Type: 20,
+		}
+		resp := e.PUT("/field/vote").
+			WithHeader("Authorization", "Bearer "+token).
+			WithCookie(CookieSecret, cookieVal).
+			WithJSON(req).Expect().Status(http.StatusOK)
+		fmt.Printf("/field/vote response: %v\n", resp.Body())
+	}
 }
 
 func testGetVotes(t *testing.T) {
