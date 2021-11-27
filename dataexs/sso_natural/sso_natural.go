6d62b08c227ad23119afea090c0ccc5e53ffec40
luxiaotong <luxiaotong713@sina.com>
2021-11-27 13:57:11 +0800

    yzt sso natural

---
+++ dataexs/sso_natural/sso_natural.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"crypto/aes"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+func encrypt(clientID, secret string) []byte {
+	ts := strconv.FormatInt(time.Now().Unix(), 10)
+	fmt.Println("ts: ", ts)
+	origData := []byte(clientID + ":" + secret + ":" + ts)
+	cipher, _ := aes.NewCipher([]byte(secret))
+	length := (len(origData) + aes.BlockSize) / aes.BlockSize
+	plain := make([]byte, length*aes.BlockSize)
+	copy(plain, origData)
+	pad := byte(len(plain) - len(origData))
+	for i := len(origData); i < len(plain); i++ {
+		plain[i] = pad
+	}
+	encrypted := make([]byte, len(plain))
+	for bs, be := 0, cipher.BlockSize(); bs <= len(origData); bs, be = bs+cipher.BlockSize(), be+cipher.BlockSize() {
+		cipher.Encrypt(encrypted[bs:be], plain[bs:be])
+	}
+	return encrypted
+}
+
+func main() {
+	host := "https://t200renzheng.zhengtoon.com"
+	clientID := "20211117001"
+	code := "d2523449-33eb-38ab-adcc-d5313bbf7a7e="
+	enc := encrypt(clientID, "31c241309a9231f585bca20c9873b49a")
+	fmt.Println("enc: ", hex.EncodeToString(enc))
+	v := url.Values{}
+	v.Set("client_id", clientID)
+	v.Set("grant_type", "authorization_code")
+	v.Set("grant_code", code)
+	v.Set("auth_token", string(enc))
+	client := &http.Client{}
+	resp, err := client.PostForm(host+"/api/oauth/getAccessToken", v)
+	if err != nil {
+		fmt.Printf("natural access token request error: %v\n", err)
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		panic(errors.Errorf("natural access token request failed: %s", resp.Status))
+	}
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	if len(respBody) == 0 {
+		panic(errors.Errorf("natural access token request invalid code: %s", code))
+	}
+	fmt.Printf("natural access token resp body: %s\n", string(respBody))
+}
+
+// online 326aef2235d22fd13d99c02172539fea86c2540f213eff6e016ca22e3f7888d733720b72e2d482f7b173114bef2f099c52252c0eded417a6ff277c70cc109d8a
+//        326aef2235d22fd13d99c02172539fea86c2540f213eff6e016ca22e3f7888d733720b72e2d482f7b173114bef2f099c6f9f9531945d5cfc6ea0ba193a83adeb
+// mememe 326aef2235d22fd13d99c02172539fea86c2540f213eff6e016ca22e3f7888d733720b72e2d482f7b173114bef2f099c6f9f9531945d5cfc6ea0ba193a83adeb
